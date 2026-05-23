e250b8540807a2117887f1584183e1de3ed958e0
anak subuh <johanesseptiawan2828@gmail.com>
2026-05-23 22:09:51 +0700

    Auto Update

--- push.bat
+++ push.bat
@@ -1,3 +1,7 @@
+@echo off
+cd /d "D:\sinau html\aruna"
 git add .
-git commit -m "first commit"
-git push -u origin main
+git commit -m "Auto Update"
+git push -u origin main --force
+echo ✅ Selesai!
+pause
